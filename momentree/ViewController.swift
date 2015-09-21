f269864b700e1602ac3789f93275cacd2a5d576d
Michael Inglis <michaelinglis93@googlemail.com>
2015-09-21 16:05:47 +0100

    Initial Commit

---
+++ momentree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  momentree
+//
+//  Created by Michael Inglis on 21/09/2015.
+//  Copyright © 2015 Michael Inglis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
