0725fd92b086e3918e2fc05908acb5800bd63986
Michael Inglis <michaelinglis93@googlemail.com>
2015-10-20 16:06:51 +0100

    Test Person

--- momentreeTests/momentreeTests.swift
+++ momentreeTests/momentreeTests.swift
@@ -13,7 +13,8 @@ class momentreeTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+
     }
     
     override func tearDown() {
@@ -22,15 +23,16 @@ class momentreeTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+        let mingles = Person(name:"mingles")
+        let stu = Person(name:"stuart")
+        var name = "nothing"
+        mingles.setDad(stu)
+        if let dad = mingles.dad {
+            name = dad.name
         }
+        
+        XCTAssertEqual("stuart", name)
+        
     }
     
 }
