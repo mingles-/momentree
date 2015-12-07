133e5d3dae1d1190cdf1f5e407d129f5e5574da4
Michael Inglis <michaelinglis93@googlemail.com>
2015-12-07 17:05:59 -0500

    Add Picker on Family Builder

--- momentree/FamilyBuilderController.swift
+++ momentree/FamilyBuilderController.swift
@@ -9,23 +9,64 @@
 import Foundation
 import UIKit
 
-class FamilyBuilderController: UIViewController {
+class FamilyBuilderController: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate {
     
     
     @IBOutlet weak var addPersonFamilyBuilderButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var fatherPicker: UIPickerView!
+    @IBOutlet weak var motherPicker: UIPickerView!
+    
+    var pickerDataSource = [Person]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fatherPicker.dataSource = self
+        fatherPicker.delegate = self
+        motherPicker.dataSource = self
+        motherPicker.delegate = self
+        
+        var tempDataSource = personArray
+        
+        let nullPerson = Person(name: "")
+        tempDataSource.append(nullPerson)
+        pickerDataSource = tempDataSource.reverse()
         
     }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count;
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerDataSource[row].name
+    }
+
     
     @IBAction func addPersonButton(sender: AnyObject) {
         print(nameTextField.text!)
         if nameTextField.text! != "" {
-            let obj = Person(name: nameTextField.text!)
-            personArray.append(obj)
-            print(personArray)
+            let newPerson = Person(name: nameTextField.text!)
+            
+            let fatherIndex = fatherPicker.selectedRowInComponent(0)
+            if pickerDataSource[fatherIndex].name != "" {
+                newPerson.setDad(pickerDataSource[fatherIndex])
+            }
+            
+            let motherIndex = motherPicker.selectedRowInComponent(0)
+            if pickerDataSource[motherIndex].name != "" {
+                newPerson.setMum(pickerDataSource[motherIndex])
+            }
+            
+            if pickerDataSource[fatherIndex].name != "" && pickerDataSource[motherIndex].name != "" {
+                pickerDataSource[motherIndex].setSpouse(pickerDataSource[fatherIndex])
+            }
+            
+            personArray.append(newPerson)
         }
         
     }
