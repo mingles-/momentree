7cc0dcde1c03d1b4efc9e9036c0c620610b896f8
Michael Inglis <michaelinglis93@googlemail.com>
2016-02-18 18:44:50 -0500

    Add Tree Slider functionality

--- momentree/FamilyViewController.swift
+++ momentree/FamilyViewController.swift
@@ -28,8 +28,20 @@ var rab = Person(name: "rab")
 var louis = Person(name: "louis")
 
 
+
+
+var ancestorSliderValues = [0,1,2,3,4]
+var descendantSliderValues = [0,1,2,3,4]
+
+var ancestorHeight = ancestorSliderValues.last!
+var descendantHeight = descendantSliderValues.last!
+
 var selectedPersonIndex = 0
-var theTree = FamilyTree(owner: personArray[selectedPersonIndex], ancestorHeight: 10, descendantHeight: 10, hasSpouse: false)
+
+var theTree = FamilyTree(owner: personArray[selectedPersonIndex], ancestorHeight: ancestorHeight, descendantHeight: descendantHeight, hasSpouse: true)
+
+
+
 
 
 class FamilyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIWebViewDelegate {
@@ -39,6 +51,16 @@ class FamilyViewController: UIViewController, UITableViewDelegate, UITableViewDa
     @IBOutlet weak var addPersonButton: UIBarButtonItem!
     @IBOutlet weak var editPersonButton: UIBarButtonItem!
     
+    @IBOutlet weak var ancestorSlider: UISlider!
+    @IBOutlet weak var ancestorSliderView: UILabel!
+    @IBOutlet weak var descendantSlider: UISlider!
+    @IBOutlet weak var descendantSliderView: UILabel!
+    
+    
+    @IBOutlet weak var spouseLabel: UILabel!
+    @IBOutlet weak var spouseSwitch: UISwitch!
+    
+
     var selectedPerson = Person(name: "")
     
     override func viewDidLoad() {
@@ -58,9 +80,22 @@ class FamilyViewController: UIViewController, UITableViewDelegate, UITableViewDa
             stuart.setParents(francis, mum: cathy)
             francis.setDad(louis)
         }
+        
         selectedPersonIndex = personArray.count-1
         selectedPerson = personArray[selectedPersonIndex]
         
+        
+        ancestorSlider.minimumValue = 0
+        ancestorSlider.maximumValue = Float(ancestorSliderValues.last!)
+        ancestorSlider.continuous = false
+        ancestorSlider.value = Float(ancestorSliderValues.last!)
+        ancestorSliderView.text = String(ancestorHeight)
+            
+        descendantSlider.minimumValue = 0
+        descendantSlider.maximumValue = Float(descendantSliderValues.last!)
+        descendantSlider.continuous = false
+        descendantSlider.value = Float(descendantSliderValues.last!)
+        descendantSliderView.text = String(descendantHeight)
         
         self.table.delegate = self
         self.table.dataSource = self
@@ -70,33 +105,90 @@ class FamilyViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         webView.loadRequest(url)
         
-        print(JSON(theTree.fullTree()))
-        
         
     }
     
     func webViewDidFinishLoad(webView: UIWebView) {
-        theTree = FamilyTree(owner: selectedPerson, ancestorHeight: 10, descendantHeight: 10, hasSpouse: false)
+        
+        theTree = FamilyTree(owner: selectedPerson, ancestorHeight: ancestorHeight, descendantHeight: descendantHeight, hasSpouse: true)
+        
+        // alter slider values based on new tree
+        ancestorSliderValues = theTree.ancestorSlider
+        descendantSliderValues = theTree.descendantSlider
+
+        ancestorSlider.maximumValue = Float(ancestorSliderValues.last!)
+        ancestorHeight = ancestorSliderValues[Int(ancestorSlider.value)]
+        ancestorSliderView.text = String(ancestorHeight)
+        
+        
+        descendantSlider.maximumValue = Float(descendantSliderValues.last!)
+        descendantHeight = descendantSliderValues[Int(descendantSlider.value)]
+        descendantSliderView.text = String(descendantHeight)
+        
+        
+        // edit color of sliders based on values
+        if ancestorSliderValues.count == 1 {
+            ancestorSlider.maximumTrackTintColor = UIColor .whiteColor()
+        } else {
+            ancestorSlider.maximumTrackTintColor = UIColor .grayColor()
+        }
+        if descendantSliderValues.count == 1 {
+            descendantSlider.maximumTrackTintColor = UIColor .whiteColor()
+        } else {
+            descendantSlider.maximumTrackTintColor = UIColor .grayColor()
+        }
+        
+        if selectedPerson.spouse == nil {
+            
+            spouseLabel.hidden = true
+            spouseSwitch.hidden = true
+            
+        } else {
+            
+            spouseLabel.hidden = false
+            spouseSwitch.hidden = false
+            
+        }
        
-//        let jsonInput = JSON([selectedPerson.fullTree(selectedPerson)]).rawString()
         let jsonInput = JSON(theTree.fullTree()).rawString()
 
         
         var textToInput = "" + jsonInput! + ""
-        
         textToInput = textToInput.stringByReplacingOccurrencesOfString("\n", withString: "")
         textToInput = textToInput.stringByReplacingOccurrencesOfString(" ", withString: "")
         let script = String(format:"document.getElementById('json-pass-in').title='%@'", textToInput)
         let script2 = String("drawGraph(true);")
         
         webView.stringByEvaluatingJavaScriptFromString(script)
-        
         webView.stringByEvaluatingJavaScriptFromString(script2)
         
     }
     
-    
-    
+    @IBAction func ancestorGenerationChanged(sender: AnyObject) {
+        
+        // snap to value
+        ancestorSlider.value = round(ancestorSlider.value)
+        
+        // set height for next FamilyTree
+        ancestorHeight = ancestorSliderValues[Int(ancestorSlider.value)]
+        
+        ancestorSliderView.text = String(ancestorHeight)
+        webView.reload()
+    }
+
+    @IBAction func descendantGenerationChanged(sender: AnyObject) {
+        
+        // snap to value
+        descendantSlider.value = round(descendantSlider.value)
+        
+        // set height for next FamilyTree
+        descendantHeight = descendantSliderValues[Int(descendantSlider.value)]
+        
+        descendantSliderView.text = String(descendantHeight)
+        
+        webView.reload()
+    }
+
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return personArray.count
