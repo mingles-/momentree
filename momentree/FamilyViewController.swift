3374a3f4503fbf9e9b2626d3f31492632d144f42
Michael Inglis <michaelinglis93@googlemail.com>
2016-03-18 21:25:07 +0000

    Remove test set for user testing

--- momentree/FamilyViewController.swift
+++ momentree/FamilyViewController.swift
@@ -12,22 +12,26 @@ import SwiftyJSON
 var personArray = [Person]()
 var momentList = [Person]()
 
-var fiona = Person(name: "fiona")
-var mingles = Person(name:"mingles")
-var stuart = Person(name:"stuart")
-var lesley = Person(name:"lesley")
+var test = Person(name: "test")
+var testDad = Person(name: "testDad")
+var testMum = Person(name: "testMum")
 
-var adam = Person(name: "adam")
-var emma = Person(name:"emma")
-var alex = Person(name: "alex")
-
-
-var francis = Person(name: "francis")
-var cathy = Person(name: "cathy")
-
-var joan = Person(name: "joan")
-var rab = Person(name: "rab")
-var louis = Person(name: "louis")
+//var fiona = Person(name: "fiona")
+//var mingles = Person(name:"mingles")
+//var stuart = Person(name:"stuart")
+//var lesley = Person(name:"lesley")
+//
+//var adam = Person(name: "adam")
+//var emma = Person(name:"emma")
+//var alex = Person(name: "alex")
+//
+//
+//var francis = Person(name: "francis")
+//var cathy = Person(name: "cathy")
+//
+//var joan = Person(name: "joan")
+//var rab = Person(name: "rab")
+//var louis = Person(name: "louis")
 
 var ancestorSliderValues = [0,1,2,3,4]
 var descendantSliderValues = [0,1,2,3,4]
@@ -67,18 +71,21 @@ class FamilyViewController: UIViewController, UITableViewDelegate, UITableViewDa
 
         
         if personArray.count == 0 {
-            personArray = [fiona, mingles, stuart, lesley, adam, emma, alex, francis, cathy, joan, rab, louis]
-            // set inital person
+            personArray = [test, testDad, testMum]
             
-            
-            // set up relationships
-            mingles.setParents(stuart, mum: lesley)
-            fiona.setParents(stuart, mum: lesley)
-            adam.setParents(alex, mum: emma)
-            lesley.setParents(rab, mum: joan)
-            emma.setParents(rab, mum: joan)
-            stuart.setParents(francis, mum: cathy)
-            francis.setDad(louis)
+//            personArray = [fiona, mingles, stuart, lesley, adam, emma, alex, francis, cathy, joan, rab, louis]
+//            // set inital person
+//            
+//            
+//            // set up relationships
+//            mingles.setParents(stuart, mum: lesley)
+//            fiona.setParents(stuart, mum: lesley)
+//            adam.setParents(alex, mum: emma)
+//            lesley.setParents(rab, mum: joan)
+//            emma.setParents(rab, mum: joan)
+//            stuart.setParents(francis, mum: cathy)
+//            francis.setDad(louis)
+                test.setParents(testDad, mum: testMum)
             
             selectedPersonIndex = personArray.count-1
         }
